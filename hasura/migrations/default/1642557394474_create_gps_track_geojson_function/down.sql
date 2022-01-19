77ec4bb96588e1ffae26f3c07f9bf9bb52915a9c
Jeffrey Canty <hi@yerffej.dev>
2022-01-18 19:59:34 -0600

    Add `geojson` computed field to `gps_track`

---
+++ hasura/migrations/default/1642557394474_create_gps_track_geojson_function/down.sql
@@ -0,0 +1,14 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION gps_track_geojson(gps_track_row gps_track)
+--  RETURNS text
+--  LANGUAGE sql
+--  STABLE
+-- AS $function$
+-- 	SELECT
+-- 		ST_AsGeoJSON(ST_MakeLine(gps_point.geom ORDER BY gps_point.timestamp))
+-- 	FROM
+-- 		gps_point
+-- 	WHERE
+-- 		gps_point.gps_track_id = gps_track_row.id
+-- $function$;
